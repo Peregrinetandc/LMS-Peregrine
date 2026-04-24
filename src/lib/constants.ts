19266c570d3c849dacebc196bf32a072b476764d
aneezv <aneesvemmulli@gmail.com>
2026-04-24 14:05:31 +0530

    feat: add constants.ts to centralize global application configuration and versioning

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,7 +5,7 @@
  */
 
 // Centralized version number for the application
-export const APP_VERSION = 'v0.1.0';
+export const APP_VERSION = 'v0.1.2';
 
 // Add any other global enterprise constants here in the future:
 // export const SUPPORT_EMAIL = 'support@peregrinehub.com';
